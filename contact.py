09c6173aeadb459263af162e4501c7d340ea38ca
Robert Redford <robertredford1970@gmail.com>
2018-07-31 23:38:26 +0300

    Ответ на третье задание (тест и вспомогательные классы)

---
+++ contact.py
@@ -0,0 +1,8 @@
+
+
+class Contact:
+
+    def __init__(self, firstname, lastname, address):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.address = address
--- test_add_contact.py
+++ test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from selenium.webdriver.firefox.webdriver import WebDriver
-from selenium.webdriver.common.action_chains import ActionChains
-import time, unittest
+import unittest
+from contact import Contact
 
 def is_alert_present(wd):
     try:
@@ -18,61 +18,57 @@ class test_add_contact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def add_new_contact(self, wd):
-        # add new contact
         wd.find_element_by_link_text("add new").click()
 
-    def fill_the_field_of_firstname(self, wd):
-        # fill the field of firstname
+    def fill_the_field_of_credentials(self, wd, contact):
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("fdsdf")
-
-    def fill_the_field_of_lastname(self, wd):
-        # fill the field of lastname
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("dsffds")
-
-    def fill_the_field_of_address(self, wd):
-        # fill the field of address
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("sdfdfs")
+        wd.find_element_by_name("address").send_keys(contact.address)
 
     def submit_the_information(self, wd):
-        # submit the information
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
     def return_to_homepage(self, wd):
-        # return to homepage
         wd.find_element_by_link_text("home page").click()
 
     def logout(self, wd):
-        # logout
         wd.find_element_by_link_text("Logout").click()
-        
+
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.add_new_contact(wd)
-        self.fill_the_field_of_firstname(wd)
-        self.fill_the_field_of_lastname(wd)
-        self.fill_the_field_of_address(wd)
+        self.fill_the_field_of_credentials(wd, Contact(firstname="abc", lastname="def", address="ghi"))
         self.submit_the_information(wd)
         self.return_to_homepage(wd)
         self.logout(wd)
 
+    def test_add_empty_contact(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.add_new_contact(wd)
+        self.fill_the_field_of_credentials(wd, Contact(firstname="", lastname="", address=""))
+        self.submit_the_information(wd)
+        self.return_to_homepage(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
