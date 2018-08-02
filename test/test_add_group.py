ae08693fc5bf6d66b0cac07a134b90fa987de014
Robert Redford <robertredford1970@gmail.com>
2018-08-03 00:13:00 +0300

    Доделать с контактами

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,11 +13,11 @@ def app(request):
 
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="1234", header="5678", footer="90"))
+    app.group.create_group(Group(name="1234", header="5678", footer="90"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.group.create(Group(name="", header="", footer=""))
+    app.group.create_group(Group(name="", header="", footer=""))
     app.session.logout()
