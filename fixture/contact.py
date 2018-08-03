1a369e37c01276bef236cd57b0db2e9e51f6bda8
Robert Redford <robertredford1970@gmail.com>
2018-08-03 22:46:48 +0300

    Ответ к задаче номер 5 (Реализовать двухуровневую архитектуру тестов)

--- fixture/contact.py
+++ fixture/contact.py
@@ -4,3 +4,30 @@ class ContactHelper:
 
     def __init__(self, app):
         self.app = app
+
+    def add_new_contact(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def fill_the_field_of_credentials(self, contact):
+        wd = self.app.wd
+        self.add_new_contact()
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("submit").click()
+        self.return_to_homepage()
+
+    def submit_the_information(self):
+        wd = self.app.wd
+        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def return_to_homepage(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,77 +1,20 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from model.contact import Contact
+from fixture.application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test_add_contact(unittest.TestCase):
-    def setUp(self):
-        self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="C:/Software-testing/Mozilla Firefox/firefox.exe")
-        self.wd.implicitly_wait(60)
+def test_add_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_the_field_of_credentials(Contact(firstname="abc", lastname="def", address="ghi"))
+    app.session.logout()
 
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def add_new_contact(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def fill_the_field_of_credentials(self, wd, contact):
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
-
-    def submit_the_information(self, wd):
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-
-    def return_to_homepage(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd)
-        self.fill_the_field_of_credentials(wd, Contact(firstname="abc", lastname="def", address="ghi"))
-        self.submit_the_information(wd)
-        self.return_to_homepage(wd)
-        self.logout(wd)
-
-    def test_add_empty_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, username="admin", password="secret")
-        self.add_new_contact(wd)
-        self.fill_the_field_of_credentials(wd, Contact(firstname="", lastname="", address=""))
-        self.submit_the_information(wd)
-        self.return_to_homepage(wd)
-        self.logout(wd)
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_add_empty_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.fill_the_field_of_credentials(Contact(firstname="", lastname="", address=""))
+    app.session.logout()
