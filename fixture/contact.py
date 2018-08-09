6567b9beebeadc0e109f948c0f71e1113c425418
Robert Redford <robertredford1970@gmail.com>
2018-08-10 01:29:52 +0300

    промежуточный результат - 10 авг

--- fixture/contact.py
+++ fixture/contact.py
@@ -25,18 +25,18 @@ class ContactHelper:
         wd.find_element_by_name("submit").click()
         self.return_to_homepage()
 
+    def change_contact_value(self, contact_field_name, text_):
+        wd = self.app.wd
+        if text_ is not None:
+            wd.find_element_by_name(contact_field_name).click()
+            wd.find_element_by_name(contact_field_name).clear()
+            wd.find_element_by_name(contact_field_name).send_keys(text_)
+
     def fill_contact_form(self, contact):
         wd = self.app.wd
         self.change_contact_value("contact firstname", contact.firstname)
         self.change_contact_value("contact lastname", contact.lastname)
         self.change_contact_value("contact address", contact.address)
-
-    def change_contact_field_value(self, contact_field_name, text):
-        wd = self.app.wd
-        if text is not None:
-            wd.find_element_by_name(contact_field_name).click()
-            wd.find_element_by_name(contact_field_name).clear()
-            wd.find_element_by_name(contact_field_name).send_keys(text)
 
     def submit_the_information(self):
         wd = self.app.wd
@@ -44,23 +44,21 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
-        wd.find_element_by_name("selected[]").click()
+        self.select_first_contact()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
 
-    def modify_first_contact(self):
+    def select_first_contact(self):
         wd = self.app.wd
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-        wd.find_element_by_xpath("//table[@id='maintable']/tbody/tr[2]/td[7]/a/img").click()
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
+        wd.find_element_by_name("selected[]").click()
+
+    def modify_first_contact(self, new_contact_data):
+        wd = self.app.wd
+        self.select_first_contact()
+        wd.find_element_by_css_selector("[title^='Edit']").click()
+        self.fill_contact_form(new_contact_data)
+        wd.find_element_by_css_selector("[title^='Update']").click()
+        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_homepage()
 
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,15 +3,15 @@ from model.contact import Contact
 
 def test_modify_contact_firstname(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Contact(firstname="New firstname"))
+    app.contact.modify_first_contact(Contact(firstname="New firstname"))
     app.session.logout()
 
 def test_modify_contact_lastname(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_contact(Contact(lastname="New lastname"))
+    app.contact.modify_first_contact(Contact(lastname="New lastname"))
     app.session.logout()
 
 def test_modify_contact_address(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify_first_group(Contact(address="New address"))
+    app.contact.modify_first_contact(Contact(address="New address"))
     app.session.logout()
