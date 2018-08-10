0238eeed183dc4ef863bce801b3852039a47e9b8
Robert Redford <robertredford1970@gmail.com>
2018-08-11 00:47:24 +0300

    Ответы к задаче №7 (Сделать все тесты создания, удаления, модификации для групп и контактов)

--- fixture/contact.py
+++ fixture/contact.py
@@ -13,30 +13,21 @@ class ContactHelper:
         wd = self.app.wd
         self.add_new_contact()
         self.fill_contact_form(contact)
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("submit").click()
         self.return_to_homepage()
 
-    def change_contact_value(self, contact_field_name, text_):
+    def change_contact_value(self, field_name, text):
         wd = self.app.wd
-        if text_ is not None:
-            wd.find_element_by_name(contact_field_name).click()
-            wd.find_element_by_name(contact_field_name).clear()
-            wd.find_element_by_name(contact_field_name).send_keys(text_)
+        if text is not None:
+            wd.find_element_by_name(field_name).click()
+            wd.find_element_by_name(field_name).clear()
+            wd.find_element_by_name(field_name).send_keys(text)
 
     def fill_contact_form(self, contact):
         wd = self.app.wd
-        self.change_contact_value("contact firstname", contact.firstname)
-        self.change_contact_value("contact lastname", contact.lastname)
-        self.change_contact_value("contact address", contact.address)
+        self.change_contact_value("firstname", contact.firstname)
+        self.change_contact_value("lastname", contact.lastname)
+        self.change_contact_value("address", contact.address)
 
     def submit_the_information(self):
         wd = self.app.wd
@@ -57,8 +48,6 @@ class ContactHelper:
         self.select_first_contact()
         wd.find_element_by_css_selector("[title^='Edit']").click()
         self.fill_contact_form(new_contact_data)
-        wd.find_element_by_css_selector("[title^='Update']").click()
-        # submit modification
         wd.find_element_by_name("update").click()
         self.return_to_homepage()
 
