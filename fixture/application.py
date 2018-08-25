cc50a8e12e735da88286df3efc1ade820f61d964
RobertRedford <robertredford1970@gmail.com>
2018-08-26 00:53:47 +0300

    Ответ к заданию 10 - "оптимизировать переходы между страницами"

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,6 @@ class Application:
 
     def __init__(self):
         self.wd = WebDriver(capabilities={"marionette": False}, firefox_binary="C:/Software-testing/Mozilla Firefox/firefox.exe")
-        self.wd.implicitly_wait(5)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
