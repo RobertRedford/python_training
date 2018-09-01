265ef967014cd95d8449bf88a0ac7d2c976fbf3d
Robert Redford <robertredford1970@gmail.com>
2018-09-01 18:20:25 +0300

    Ответ к Заданию номер 12: Реализовать кеширование при загрузке списков групп и контактов

--- fixture/contact.py
+++ fixture/contact.py
@@ -20,6 +20,7 @@ class ContactHelper:
         self.fill_contact_form(contact)
         wd.find_element_by_name("submit").click()
         self.return_to_homepage()
+        self.contact_cache = None
 
     def change_contact_value(self, field_name, text):
         wd = self.app.wd
@@ -43,6 +44,7 @@ class ContactHelper:
         self.select_first_contact()
         wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
+        self.contact_cache = None
 
     def select_first_contact(self):
         wd = self.app.wd
@@ -55,6 +57,7 @@ class ContactHelper:
         self.fill_contact_form(new_contact_data)
         wd.find_element_by_name("update").click()
         self.return_to_homepage()
+        self.contact_cache = None
 
     def return_to_homepage(self):
         wd = self.app.wd
@@ -65,22 +68,15 @@ class ContactHelper:
         self.open_contacts_page()
         return len(wd.find_elements_by_name("selected[]"))
 
-    def get_contact_list(self):
-        wd = self.app.wd
-        self.open_contacts_page()
-        contacts = []
-        for element in wd.find_elements_by_name("entry"):
-            cells = element.find_elements_by_tag_name("td")
-            id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-            contacts.append(Contact(lastname=cells[1].text, firstname=cells[2].text, id=id))
-        return contacts
+    contact_cache = None
 
-        # wd = self.app.wd
-        # self.open_contacts_page()
-        # contacts = []
-        # for element in wd.find_elements_by_css_selector("tr[name=entry]"):
-        #     id = element.find_elements_by_name("selected[]").get_attribute("value")
-        #     firstname = element.find_elements_by_css_selector("td:nth-child(2)").textContent
-        #     lastname = element.find_elements_by_css_selector("td:nth-child(3)").textContent
-        #     contacts.append(Contact(name=text, id=id))
-        # return contacts
+    def get_contact_list(self):
+        if self.contact_cache is None:
+            wd = self.app.wd
+            self.open_contacts_page()
+            self.contact_cache = []
+            for element in wd.find_elements_by_name("entry"):
+                cells = element.find_elements_by_tag_name("td")
+                id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                self.contact_cache.append(Contact(lastname=cells[1].text, firstname=cells[2].text, id=id))
+        return list(self.contact_cache)
