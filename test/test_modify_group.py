2cf0dc7a12d186b7a2aafb8700cda553a6585a45
RobertRedford <robertredford1970@gmail.com>
2018-09-02 23:40:46 +0300

    промежуточный итог (13 задание)

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,23 +1,28 @@
 from model.group import Group
+from random import randrange
 
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Modify test name"))
     old_groups = app.group.get_group_list()
+    index = randrange(len(old_groups))
     group = Group(name="New group")
-    group.id = old_groups[0].id
-    app.group.modify_first_group(group)
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
-    old_groups[0] = group
+    old_groups[index] = group
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
         app.group.create(Group(header="Modify test header"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(header="New header"))
+    index = randrange(len(old_groups))
+    group = Group(header="New header")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
 
@@ -25,6 +30,9 @@ def test_modify_group_footer(app):
     if app.group.count() == 0:
         app.group.create(Group(footer="Modify test header"))
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(footer="New footer"))
+    index = randrange(len(old_groups))
+    group = Group(footer="New footer")
+    group.id = old_groups[index].id
+    app.group.modify_group_by_index(index, group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
