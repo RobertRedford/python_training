72808d30a7746c8fba28ae6286867254fe392e40
Robert Redford <robertredford1970@gmail.com>
2018-09-08 18:34:30 +0300

    тесты на email, адрес (не доделан)

--- model/contact.py
+++ model/contact.py
@@ -3,7 +3,9 @@ from sys import maxsize
 
 class Contact:
 
-    def __init__(self, firstname=None, lastname=None, home=None, mobile=None, work=None, phone2=None, id=None, all_phones_from_home_page=None):
+    def __init__(self, firstname=None, lastname=None, home=None, mobile=None, work=None, phone2=None, id=None,
+                 email=None, email2=None, email3=None, address=None,
+                 all_phones_from_home_page=None, all_emails_from_home_page=None):
         self.firstname = firstname
         self.lastname = lastname
         self.home = home
@@ -11,7 +13,12 @@ class Contact:
         self.work = work
         self.phone2 = phone2
         self.id = id
+        self.address = address
+        self.email = email
+        self.email = email2
+        self.email = email3
         self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
         return "%s: %s: %s " % (self.firstname, self.lastname, self.id)
