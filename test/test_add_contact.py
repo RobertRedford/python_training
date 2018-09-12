b432828e434ff8e3af5def6621a400f40ef70331
Robert Redford <robertredford1970@gmail.com>
2018-09-13 00:06:37 +0300

    Ответ к 15 заданию (Параметризовать тесты для создания групп и контактов)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,29 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
-def test_add_contact(app):
+testdata = [Contact(firstname="", lastname="")] + [
+    Contact(firstname=random_string("firstname", 10), lastname=random_string("lastname", 20))
+    for i in range(5)
+
+]
+
+# testdata = [
+#     Contact(firstname=firstname, lastname=lastname)
+#     for firstname in ["", random_string("firstname", 10)]
+#     for lastname in ["", random_string("lastname", 20)]
+#
+#  ]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
     contact = Contact(firstname="1234", lastname="5678")
     app.contact.fill_the_field_of_credentials(contact)
@@ -11,11 +32,11 @@ def test_add_contact(app):
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
-def test_add_empty_contact(app):
-    old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="", lastname="")
-    app.contact.fill_the_field_of_credentials(contact)
-    new_contacts = app.contact.get_contact_list()
-    assert len(old_contacts) + 1 == len(new_contacts)
-    old_contacts.append(contact)
-    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+# def test_add_empty_contact(app):
+#     old_contacts = app.contact.get_contact_list()
+#     contact = Contact(firstname="", lastname="")
+#     app.contact.fill_the_field_of_credentials(contact)
+#     new_contacts = app.contact.get_contact_list()
+#     assert len(old_contacts) + 1 == len(new_contacts)
+#     old_contacts.append(contact)
+#     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
