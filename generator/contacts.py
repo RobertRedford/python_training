d6253e3a0cd7d693262f7b36ebc0fe7359c135b8
Robert Redford <robertredford1970@gmail.com>
2018-09-22 03:16:00 +0300

    Ответ к Задание №18: Реализовать генераторы тестовых данных

---
+++ generator/contacts.py
@@ -0,0 +1,40 @@
+from model.contact import Contact
+import random
+import string
+import os.path
+import json
+import getopt
+import sys
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f:", ["number of groups", "file"])
+except getopt.GetoptError as err:
+    # print help information and exit:
+    print(err)  # will print something like "option -a not recognized"
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+testdata = [Contact(lastname="", firstname=""] + [
+    Contact(firstname=random_string("firstname", 20), lastname=random_string("lastname", 15))
+    for i in range(n)
+
+]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
