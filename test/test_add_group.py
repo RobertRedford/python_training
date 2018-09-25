3b73588c86faaee26cc3cbd105d32d190721ee13
Robert Redford <robertredford1970@gmail.com>
2018-09-26 02:23:27 +0300

    Ответ к 29 заданию - Реализовать генерацию отчётов при помощи Allure

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from model.group import Group
-# import pytest
+import pytest
 # from data.groups import constant as testdata
 
 
@@ -16,12 +16,15 @@ from model.group import Group
 # @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, json_groups):
     group = json_groups
-    old_groups = app.group.get_group_list()
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    with pytest.allure.step('Given a group list'):
+        old_groups = app.group.get_group_list()
+    with pytest.allure.step('When I add a group %s list' % group):
+        app.group.create(group)
+    with pytest.allure.step('Then the new group list is equal to the old list with added group'):
+        new_groups = app.group.get_group_list()
+        assert len(old_groups) + 1 == len(new_groups)
+        old_groups.append(group)
+        assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 # def test_add_empty_group(app):
 #     old_groups = app.group.get_group_list()
